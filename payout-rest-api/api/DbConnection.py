5aab189750e27c7eba980661ffa8f82184ee2a13
tbriot <thomas.briot82@gmail.com>
2018-03-12 20:44:58 -0400

    check security and duplicate in db

--- payout-rest-api/api/DbConnection.py
+++ payout-rest-api/api/DbConnection.py
@@ -1,5 +1,7 @@
 import MySQLdb
 import os
+from datetime import datetime
+from decimal import Decimal
 
 
 class DbConnection:
@@ -15,27 +17,63 @@ class DbConnection:
                                     )
 
     def insert_payout(self, payout):
+        self.validate_payout(payout)
+        payout = self.convert_type(payout)
+
         cur = self.conn.cursor()
+        sec = self.find_security(
+            cur,
+            payout['exchange_code'],
+            payout['security_symbol'],
+            )
+
+        if sec is None:  # if security was not found in db
+            raise SystemError(
+                "Unknown security. exchange_code={} "
+                "security_symbol={}".format(
+                    payout['exchange_code'],
+                    payout['security_symbol']
+                )
+            )
+
+        del payout['exchange_code']
+        del payout['security_symbol']
+        payout['security_id'] = sec['security_id']
+        print(payout)
+
+        p = self.find_payout_close_to_paydate(
+                cur,
+                sec['security_id'],
+                payout['pay_date']
+            )
+
+        del p['payout_id']
+        print(p)
+        if p is not None:  # if payout was found w/ paydate +/- 1 week
+            if p == payout:
+                raise SystemError("Duplicate payout")
+
+            else:
+                raise SystemError("Ambiguous payout. May be a duplicate")
+
         stmt = self.get_insert_payout_stmt(payout)
         cur.execute(stmt, payout.values())
         self.conn.commit()
 
-    def find_security(self, exchange_code, security_symbol):
-        cur = self.conn.cursor()
+    def find_security(self, cursor, exchange_code, security_symbol):
         stmt = self.get_select_security_stmt(exchange_code, security_symbol)
-        cur.execute(stmt, [exchange_code, security_symbol])
-        data = cur.fetchone()
+        cursor.execute(stmt, [exchange_code, security_symbol])
+        data = cursor.fetchone()
         if data is not None:
             return {'security_id': data[0],
                     'exchange_id': data[1]}
 
-    def find_payout_close_to_paydate(self, security_id, pay_date):
-        cur = self.conn.cursor()
+    def find_payout_close_to_paydate(self, cursor, security_id, pay_date):
         stmt = self.get_select_payout_paydate_stmt(security_id, pay_date)
-        cur.execute(stmt, [security_id, pay_date, pay_date])
-        data = cur.fetchone()
+        cursor.execute(stmt, [security_id, pay_date, pay_date])
+        data = cursor.fetchone()
         if data is not None:
-            cols_name = [i[0] for i in cur.description]
+            cols_name = [i[0] for i in cursor.description]
             return dict(zip(cols_name, data))
 
     @staticmethod
@@ -76,3 +114,25 @@ class DbConnection:
             }
         except KeyError as e:
             print("Environment variable is not set: %s" % str(e))
+
+    @staticmethod
+    def validate_payout(payout):
+        assert payout.get('exchange_code'), \
+            "'exchange_code' param is missing"
+        assert payout.get('security_symbol'), \
+            "'security_symbol' param is missing"
+        assert payout.get('pay_date'), \
+            "'spay_date' param is missing"
+
+    @staticmethod
+    def convert_type(payout):
+        payout['declared_date'] = datetime.strptime(payout['declared_date'],
+                                                    '%Y-%m-%d').date()
+        payout['record_date'] = datetime.strptime(payout['record_date'],
+                                                  '%Y-%m-%d').date()
+        payout['ex_date'] = datetime.strptime(payout['ex_date'],
+                                              '%Y-%m-%d').date()
+        payout['pay_date'] = datetime.strptime(payout['pay_date'],
+                                               '%Y-%m-%d').date()
+        # payout['net_amount'] = Decimal(payout['net_amount'])
+        return payout
--- payout-rest-api/api/RestApi.py
+++ payout-rest-api/api/RestApi.py
@@ -8,22 +8,20 @@ db = 'dividend'
 '''
 
 payout = {
-    'exchange': 'NYSE',
-    'symbol': 'MMM',
+    'exchange_code': 'NASDAQ',
+    'security_symbol': 'ATVI',
     'declared_date': '2018-03-12',
     'record_date': '2018-04-02',
     'ex_date': '2018-04-03',
     'pay_date': '2018-04-23',
     'net_amount': 0.25,
     'currency_code': 'USD',
-    'frequency': 'MONTHLY'
+    'frequency': 'MONTHLY',
+    'payment_type': None,
+    'qualified': None,
+    'flag': None
 }
 
 conn = DbConnection()
-res = conn.find_payout_close_to_paydate(2, '2018-04-20')
-if res is not None:
-    print("Entry with close paydate found")
-    del res['payout_id']
-    print(res)
-# res = conn.find_security(payout['exchange'], payout['symbol'])
+res = conn.insert_payout(payout)
 # conn.insert_payout(payout)
